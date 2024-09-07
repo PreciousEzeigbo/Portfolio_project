bcfca7dec5a56d3bfa49bc542a1ed9f644c52c49
PreciousEzeigbo <preciousezeigbo81@gmail..com>
2024-09-07 19:14:38 +0100

    Update

--- blueprintapp/models.py
+++ blueprintapp/models.py
@@ -7,8 +7,11 @@ class User(db.Model, UserMixin):
 
     uid = db.Column(Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String, nullable=False)
-    
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+
     def __repr__(self):
         return f'<User: {self.username}'
 
