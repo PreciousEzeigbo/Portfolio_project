8b887f1888c96eb426470bf485a6c7590f95a583
PreciousEzeigbo <preciousezeigbo81@gmail..com>
2024-09-08 23:05:11 +0100

    Updating

--- blueprintapp/routes.py
+++ blueprintapp/routes.py
@@ -23,6 +23,13 @@ def register_routes(app, db, bcrypt):
             return render_template('login.html')
         elif request.method == 'POST':
             pass
+
+    @app.route('/forgotten', methods=['GET', 'POST'])
+    def forgotten():
+        if request.method == 'GET':
+            return render_template('forgotten.html')
+        elif request.method == 'POST':
+            pass
     
     @app.route('/logout')
     def logout():
